b23c71ccd1b7d8f485193d9efe45e12394b13f58
jgp <jiangguiping@shensizaowu.com>
2026-01-07 15:09:31 +0800

    feat: 增加meta

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { webpack }) => {
+    // 使用 IgnorePlugin 忽略 @mapbox/node-pre-gyp 中的 HTML 文件
+    config.plugins = config.plugins || [];
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /@mapbox\/node-pre-gyp\/.*\.html$/,
+      })
+    );
+    return config;
+  },
+  turbopack: {},
 };
 
 export default nextConfig;
